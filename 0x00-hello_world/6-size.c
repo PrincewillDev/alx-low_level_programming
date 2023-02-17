3b8a919fb4d3805405ab3ac068fa30d84fccd9e0
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-17 11:52:18 -0800

    Fixed issues in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long longint: %lu byte(s)\n", (unsigned long)sizeof(li2));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(li2));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 
 	return (0);
