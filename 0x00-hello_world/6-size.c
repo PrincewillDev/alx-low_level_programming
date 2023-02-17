b0a3cca9a6caa47c110ae9b4bc57cd3a6ed4ed77
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-17 11:34:25 -0800

    Fixed issues in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,16 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
+	long int longint;
+	long long int longint2;
 	char charType;
 
 	/* sizeof evealuates the size of a variable */
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of int: %zu bytes\n", sizeof(floatType));
-	printf("Size of int: %zu bytes\n", sizeof(doubleType));
-	printf("Size of int: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %lu bytes\n", (unsigned long)sizeof(intType));
+	printf("Size of float: %lu bytes\n", (unsigned long)sizeof(floatType));
+	printf("Size of long_int: %lu bytes\n", (unsigned long)sizeof(longint));
+	printf("Size of long_longint: %lu bytes\n", (unsigned long)sizeof(longint2));
+	printf("Size of char: %lu bytes\n", (unsigned long)sizeof(charType));
 
 	return (0);
 }
