cadb6e7b1de0ab84ebdfef9e0a319e4babf8c50c
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-20 15:48:44 -0800

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+
+		letter++;
+
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+
+		putchar(letter);
+
+		letter++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
