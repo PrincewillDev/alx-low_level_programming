5d5fb69e136ff366e24e584666282984e721a391
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-20 17:16:15 -0800

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base_num;
+
+	base_num = 0;
+
+	for (base_num = 0; base_num < 10; base_num++)
+
+		putchar("%d", base_num);
+
+	putchar("\n");
+
+	return (0);
+
+}
