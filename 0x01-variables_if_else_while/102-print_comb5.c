34be8affccff7dc17913bfacb134e5f31fe8b21f
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-21 14:27:20 -0800

    finished task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,35 @@
+
 #include <stdio.h>
 
 /**
- * main -entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i, j;
 
 	for (i = 0; i <= 99; i++)
 	{
-	for (j = i; j <= 99; j++)
-	{
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		putchar(' ');
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
-		if (i != 99 || j != 99)
+		for (j = 0; j <= 99; j++)
 		{
-		putchar(',');
-		putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-	}
-
+	putchar(10);
 	return (0);
 }
