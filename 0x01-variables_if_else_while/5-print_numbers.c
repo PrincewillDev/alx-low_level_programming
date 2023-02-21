14712b37dc145af704c33355a980263bfb114ce3
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-20 17:01:11 -0800

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base_num;
+
+	base_num = 0;
+
+	for (base_num = 0; base_num < 10; base_num++)
+
+		printf("%d", base_num);
+
+	return (0);
+
+}
