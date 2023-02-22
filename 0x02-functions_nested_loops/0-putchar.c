828c6d31f0584c66d6fae00a941caa62e56b509a
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-22 10:25:45 -0800

    Added new project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: output putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char output = "_putchar";
+
+	putchar(output);
+
+	return (0);
+}
