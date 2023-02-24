2e1a98354fdd488f8da3135a7a75df900173882c
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-24 13:43:47 -0800

    added a new task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer variable
+ * @b: integer variable
+ * Return: 0 always (Success)
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
