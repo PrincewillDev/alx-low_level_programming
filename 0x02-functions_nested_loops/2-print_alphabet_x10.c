7bb6ff2f3b956ef9d6e0103e51cfe93d5c0f31f8
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-25 14:16:06 -0800

    added task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - The function prints out lowercase  alphabet 10 times
+ * void: void function
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	char x;
+
+	while (i < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+
+		i++;
+	}
+
+}
