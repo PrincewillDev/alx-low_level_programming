e5b5ed087679d409e65f136a8d2a215cb9c2a72b
PrincewillDev <princewillfidelis1@gmail.com>
2023-02-27 16:02:41 -0800

    Added new tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that uses a pointer to update the value to 98
+ *
+ * @n: the pointer for the function parameter
+ *
+ * void - its a void datatype
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
