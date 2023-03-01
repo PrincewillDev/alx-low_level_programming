1cefa48ed3a3ea7aee605bc4dd24f8b3e7085e52
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-01 15:03:15 -0800

    added task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - this functions copies the source string to the destination string
+ *
+ * @dest: the dest pointer pointing to where the copied string is stored
+ * @src: the src pointer pointing to the source string to be copied
+ * @n: n parameter represent the number of time the source will be appended
+ *
+ * Return: pointer dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	strncpy(char *dest, char *src, n);
+
+	return (dest;)
+}
