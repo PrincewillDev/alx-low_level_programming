85730af3d2be7c78c53392503e08be849fe07f80
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-01 11:42:44 -0800

    Adding tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - This function concatenate two strings
+ *
+ * @dest: The destination pointer parameter
+ * @src: The source pointer parameter
+ * char - The char datatype
+ * Return: the destination pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	strcat(dest, src);
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+### 0-strcat.c
+This Program creates  a function that concatenates two strings.
