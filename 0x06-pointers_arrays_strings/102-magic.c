7ec1e5e9cabde7d701d55efa42b602903862e49e
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-05 09:45:52 -0800

    fixed errors

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - the main function prints out the integer 98
+ * Return: 0 Always (Success)
+ */
+
 int main(void)
 {
-
 	int n;
 
 	int a[5];
@@ -12,7 +16,7 @@ int main(void)
 	a[2] = 1024;
 
 	p = &n;
-	*(p + 5) = 98;
+
   /*
    * write your line of code here...
    * Remember:
@@ -22,7 +26,7 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
 
-
+	*(p + 5) = 98;
   /* ...so that this prints 98\n */
 
 	printf("a[2] = %d\n", a[2]);
