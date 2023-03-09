c975af090573663f580d199ef5a51c0bdf5797ae
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-08 17:55:23 -0800

    fixed betty issues

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,23 +7,39 @@
  *
  * Return: 1 if the integer is a prime number, otherwise return 0.
  */
+
 int is_prime_number(int n)
 {
-    int i;
 
-    /* Check if n is less than 2, which is not a prime number. */
-    if (n < 2) {
-        return 0;
-    }
+	int i;
 
-    /* Check if n is divisible by any integer from 2 to sqrt(n). */
-    for (i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
 
-    /* If n is not divisible by any integer from 2 to sqrt(n), then it is a prime number. */
-    return 1;
+	/* Check if n is less than 2, which is not a prime number. */
+
+	if (n < 2)
+	{
+
+		return (0);
+
+	}
+
+	/* Check if n is divisible by any integer from 2 to sqrt(n). */
+
+	for (i = 2; i * i <= n; i++)
+	{
+
+		if (n % i == 0)
+		{
+
+			return (0);
+
+		}
+
+	}
+
+
+	/* If n is not divisible by a num from 2 to sqrt(n),its a prime num */
+
+	return (1);
 }
 
