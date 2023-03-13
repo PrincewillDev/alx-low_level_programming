08d78da4bc2c8d0808dc949e5d8229f529d60e1d
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-13 16:21:03 -0700

    added new task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the number of commandline argument
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 always (success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc);
+	return (0);
+}
