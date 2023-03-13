493536e9a5081a80744385691cb670e82af73a79
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-13 16:04:32 -0700

    fixed issues

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,22 +10,7 @@
 int main(int argc, char *argv[])
 {
 
-	if (argc == 0)
+	printf("%s\n", argv[argc * 0]);
 
-	{
-
-		return (0);
-
-	}
-
-	int i;
-
-
-	for (i = 0; i < argc; i++)
-
-	{
-
-		printf("%s\n", argv[i]);
-
-	}
+	return (0);
 }
