72a57d49a5d189c2e0da9c3fa0ec0ea3ca9ffa26
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-14 16:58:05 -0700

    added new task

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 int _putchar(char c);
 int main (int argc, char *argv[]);
 int main(int argc, __attribute__((unused)) char *argv[]);
+int main (int argc, char *argv[]);
 int main (int argc, char *argv[]);
 
 #endif /*MAIN_H */
