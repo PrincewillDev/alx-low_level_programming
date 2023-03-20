7642bbe448d0c7505f96dcb08b85a1f6b8a7c283
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-20 16:56:26 -0700

    fixed error

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
  * main - print the name of the source file in the command line
- * @argc: n args
- * @argv: arr args
  * Return: 0 success
  */
 
 int main(void)
 {
-	char *filename = __FILE__;
+	printf("%s\n", __FILE__);
 
-	printf("%s\n", filename);
-
-	return(0);
+	return (0);
 }
