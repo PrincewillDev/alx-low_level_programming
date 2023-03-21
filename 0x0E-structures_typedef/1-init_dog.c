d88beada8dfb80b6094b8ac9c11aa35c8d56d7f8
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-21 15:54:03 -0700

    fixed the issue

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,9 +14,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	(*d).name = name;
-
-	d->age = age;
-
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
