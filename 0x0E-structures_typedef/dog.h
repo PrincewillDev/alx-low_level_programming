4ab46290a4c0089a527bf95bf1b7492fd1f277f9
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-21 17:16:24 -0700

    add new task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dog - represents a dog with a name, age, and owner
  * @name: the name of the dog
@@ -20,5 +24,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
