c759565450f717e8386cb9d7751fa851be3868b1
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-23 16:45:05 -0700

    fixed task error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,34 +9,22 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 
-	va_list args;
 	unsigned int i;
 	char *str;
+	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-
 		str = va_arg(args, char *);
-
-		if (str == NULL)
-		{
-
-			printf("nil");
-		}
-
-		else
-		{
-
+		if (str != NULL)
 			printf("%s", str);
-		}
+		else
+			printf("(nil)");
 
 		if (i != (n - 1) && separator != NULL)
-		{
-
 			printf("%s", separator);
-		}
 	}
 
 	printf("\n");
