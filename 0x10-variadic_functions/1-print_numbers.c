ec1c69c57922e23d24a75ce07529d26d443bd957
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-23 15:27:48 -0700

    add new task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints out numbers
+ * @separator: string pointer
+ * @n: unsigned int count argument
+ * return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	va_list args;
+
+	unsigned int i;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+
+		int num = va_arg(args, int);
+
+		printf("%d", num);
+
+		if (i != n - 1)
+		{
+			printf("%s", separator);
+		}
+
+
+	}
+
+	printf("\n");
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,6 @@
 /* Prototypes  */
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H  */
