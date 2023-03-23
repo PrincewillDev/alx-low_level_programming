d7bb0749fa1382e5b9e41c213dbd710abdb9775d
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-23 15:43:09 -0700

    fixed task error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", num);
 
-		if (i != n - 1)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
