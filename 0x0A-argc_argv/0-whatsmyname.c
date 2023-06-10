cd1802d270ba858f61c892d6eb7e82e0041df32d
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-10 13:59:05 -0700

    Update Repo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * Return: 0 (success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
