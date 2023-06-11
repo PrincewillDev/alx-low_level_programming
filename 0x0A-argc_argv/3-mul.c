eda353767b5fd37168dd16f3a6b45dc30b3a89d0
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-11 09:21:10 -0700

    fixed errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,23 +11,19 @@
 int main(int argc, char *argv[])
 {
 
-	int i, mul;
+	int num1, num2, mul;
 
-	mul = 1;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			mul = mul *  atoi(argv[i]);
-		}
-
-		printf("%d\n", mul);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
 
 	return (0);
 }
