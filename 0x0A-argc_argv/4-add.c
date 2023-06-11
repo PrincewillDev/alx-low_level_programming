5dba35b39952c28c8fc3423e6acdcc760db1cc7e
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-11 16:00:31 -0700

    Update code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,27 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
-	if (argc == 0 )
+	if (argc == 0)
 	{
 		printf("0\n");
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum +=  atoi(argv[i]);
 
-		else
-		{
-			sum = sum + atoi(argv[i]);
-		}
 	}
 
 	printf("%i\n", sum);
