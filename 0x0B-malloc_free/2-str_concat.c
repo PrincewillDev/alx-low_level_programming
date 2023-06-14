19591d2c0d7c69ca0916d013de1852f2990ac607
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-14 14:47:22 -0700

    Update repo

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * str_concat - This function concatenate two strings
+ * @s1: destination argument
+ * @s2: source argument
+ * Return: pointer to malloc.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	unsigned int len = 0;
+	int i, j, s;
+	char *str;
+
+	s = 0;
+	i = 0;
+	j = 0;
+
+	while (s1[len] != '\0' && s2[len] != '\0')
+	{
+		len++;
+	}
+
+	str = malloc(len * sizeof(char) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1[i] != '\0')
+	{	str[s] = s1[i];
+		i++;
+		s++;
+	}
+
+	while (s2[j] != '\0')
+	{
+
+		str[s] = s2[j];
+		j++;
+		s++;
+	}
+
+	str[s] = '\0';
+
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,3 +5,8 @@ Prototype: char *create_array(unsigned int size, char c);
 Returns NULL if size = 0
 Returns a pointer to the array, or NULL if it fails
 
+### 1-strdup.c
+This function returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+### 2-str_concat.c
+This function concatenate two strings.
