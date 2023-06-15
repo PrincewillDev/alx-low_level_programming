68c1df440a62e8af0ea72cac882b210ec48910b5
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-15 15:26:04 -0700

    Update repo

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate two string
+ * @s1: destination pointer
+ * @s2: source pointer
+ * @n: nth value
+ * Return: pointer to malloc
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{	char *ptr;
+	unsigned int len1 = 0, len2 = 0, i, j, s;
+
+	i = 0;
+	s = 0;
+
+	if (s1 == NULL)
+	{return (NULL);
+	}
+	if (s2 == NULL)
+	{return (NULL);
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		ptr[s] = s1[i];
+		s++;
+		i++;
+	}
+	for (j = 0, s = i; j < n; j++, s++)
+	{
+		ptr[s] = s2[j];
+	}
+	ptr[s] = '\0';
+	return (ptr);
+}
