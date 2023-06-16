965b4a33ede9947928f8a9e13f38cc33da1d3f42
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-16 15:47:33 -0700

    Update repo

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of int
+ * @min: min value argument
+ * @max: max value argument
+ * Return: pointer to mem for array range
+ */
+
+int *array_range(int min, int max)
+{
+
+	int *ptr;
+	int i;
+
+	if (min > max)
+	{
+
+		return (NULL);
+	}
+
+	ptr = malloc((max - min + 1) * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		ptr[i] = i;
+	}
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,8 @@
 ### 0-malloc_checked.c
 This function allocates memory using malloc.
+### 1-string_nconcat.c
+This function concatenates two strings.
+###  2-calloc.c
+This function  allocates memory for an array, using malloc.
+### 3-array_range.c
+This function creates an array of integers.
