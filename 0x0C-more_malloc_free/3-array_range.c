489d6d0d274314922123780900789819441761d9
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-16 16:12:34 -0700

    Fixed errors

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 
 		return (NULL);
 	}
-	
+
 	size = (max - min) + 1;
 
 	ptr = malloc(size * sizeof(int));
@@ -27,9 +27,9 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	for (i = min; i <= max; i++)
+	for (i = 0; i < size; i++)
 	{
-		ptr[i] = i;
+		ptr[i] = min + i;
 	}
 
 	return (ptr);
