4c5408f6e46008d1dfdb899472520e5baee9b9a6
PrincewillDev <princewillfidelis1@gmail.com>
2023-06-21 15:28:00 -0700

    Fixed errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,21 +8,12 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	unsigned int i;
-
-	i = 0;
-
-	if (name == NULL || name == NULL)
+	if (name == NULL || f == NULL)
 	{
 		return;
 	}
 
-	while (name[i] != '\0')
-	{
-		i++;
-	}
-
-	_putchar('\0');
-
 	f(name);
+
 }
+
