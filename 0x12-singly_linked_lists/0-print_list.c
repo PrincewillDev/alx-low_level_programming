1527cea52735b0f89fca45e83f40954714517936
PrincewillDev <princewillfidelis1@gmail.com>
2023-07-03 16:52:33 -0700

    Update repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,22 @@
 /**
  * print_list - this function prints the values of the list
  * @h: pointer arg
- * Return: t
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t t = 0;
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
-
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		t++;
 	}
-
 	return (t);
 }
