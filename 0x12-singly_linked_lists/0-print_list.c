3035f0d288b2e9bfa3b3966f9ed9abdb16220d39
PrincewillDev <princewillfidelis1@gmail.com>
2023-07-03 17:00:39 -0700

    Update repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,12 @@
 size_t print_list(const list_t *h)
 {
 	size_t t = 0;
-
-	if (h == NULL)
+	while (h)
 	{
-		printf("[0] (nil)\n");
-		return (0);
-	}
-
-	while (h != NULL)
-	{
-		printf("[%u] %s\n", h->len, h->str);
+		printf("[%lu] %s\n", h->str ? strlen(h->str) : 0, h->str ? h->str : "(nil)");
 		h = h->next;
 		t++;
 	}
+
 	return (t);
 }
