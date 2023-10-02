9f2594f2719be9283d3b93fef0ed087de4b10e3f
PrincewillDev <princewillfidelis1@gmail.com>
2023-10-02 16:19:19 -0700

    fixed error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,41 +6,25 @@
  * @text_content: file content
  * Return: returns 1 on success, -1 if failed
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_des;
-	int len, i;
-	void *buf;
+	int file_des, i, w;
 
-	while (text_content != NULL)
-	{
-		len++;
-	}
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	buf = malloc(len);
-	/* Opened the file in append mode */
-	file_des = open(filename, O_WRONLY | O_APPEND);
-	if (file_des == -1)
-	{
+	file_des = open(filename, O_EXCL | O_APPEND | O_WRONLY);
+	if (file_des < 0)
 		return (-1);
-	}
-	if (text_content == NULL)
+	if (text_content)
 	{
-		close(file_des);
-		return (1);
+		i = 0;
+		while (text_content[i] != '\0')
+			i++;
+		w = write(file_des, text_content, i);
+		if (w < 0)
+			return (-1);
 	}
-	/* Process the opened file, by appending content to it */
-	for (i = 0; i < len; i++)
-	{
-		if ((write(file_des, buf, len)) != -1)
-		{
-			return (1);
-		}
-	}
-	free(buf);
 	close(file_des);
-	return (0);
+	return (1);
 }
