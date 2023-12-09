6e17a4df052d65c28fea2a30851dabfd4bae26ec
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-09 14:34:38 +0100

    feat: add a new task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * dlistint_len - Return the amount of element in list
+ * @h: pointer to head
+ * Return: number of element
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int size = 0;
+	
+	temp = h;
+	while (temp != NULL)
+	{
+		size++;
+		temp = temp -> next;
+	}
+
+	return (size);
+}
