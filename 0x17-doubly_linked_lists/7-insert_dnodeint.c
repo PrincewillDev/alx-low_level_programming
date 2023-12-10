27e8d756ad013b9a88d4ee1f5d9a9207fda72c7e
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-10 18:52:55 +0100

    feat: add node at index point

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: pointer to head
+ * @idx: node index
+ * @n: the data in node
+ * Return: newnode on success and NULL on failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *ftemp, *stemp;
+	unsigned int count = 0;
+
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	ftemp = *h;
+	while (ftemp != NULL)
+	{
+		stemp = ftemp->next;
+		if (stemp == NULL)
+		{
+			return (NULL);
+		}
+		if (count == idx - 1)
+		{
+			newnode->n = n;
+			newnode->prev = ftemp;
+			newnode->next = stemp;
+			ftemp->next = newnode;
+			stemp->prev = newnode;
+		}
+		ftemp = ftemp->next;
+		count++;
+	}
+	if (count != idx)
+	{
+		return (NULL);
+	}
+	return (newnode);
+
+}
