e57b9e7ecf42cca77a4f51119e0b4b68983f3401
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-11 22:37:23 +0100

    feat: Create a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/math_function.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - addition of numbers
+ * @a: first arg
+ * @b: second arg
+ * Return: Sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction of numbers
+ * @a: first arg
+ * @b: second arg
+ * Return: Subtract of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication of numbers
+ * @a: first arg
+ * @b: second arg
+ * Return: multiply of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of numbers
+ * @a: first arg
+ * @b: second arg
+ * Return: divide of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of numbers
+ * @a: first arg
+ * @b: second arg
+ * Return: remainder of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
