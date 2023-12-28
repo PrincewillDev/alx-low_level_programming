a675ab3b500b22a1bd497cced13b493f702a75b6
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-28 17:29:46 +0100

    feat: add a hash function that uses the djb2 algorithm to hash a key

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 is a fast and simple hash function
+ * @str: a pointer to the key
+ * Return: the hash value to be distributed
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	/* Initialize hash to 5381 */
+	unsigned int hash = 5381;
+
+	/* declare an int var to get the ascii value of the string (key)*/
+	unsigned int c;
+
+	/* use a loop to iterate through the string (key) to hash the key*/
+	while ((c = *str++))
+	{
+		/* The hashing formula to hash the key*/
+		hash = ((hash << 5) + hash) + c; 
+	}
+
+	return (hash);
+}
