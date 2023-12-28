bc3a99207a76c3bc86203966dc5a647fcef7c1b7
Princewill Elebhose <princewillfidelis1@gmail.com>
2023-12-28 01:37:11 +0100

    fix: allocated memory with calloc for the array pointer to node in hash table struct

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t  *hash_table;
+	unsigned int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
+		free(hash_table);
 		return (NULL);
 	}
 
 	hash_table->size = size;
+	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table->array);
+		return (NULL);
+	}
 
-	free(hash_table);
+	for (i = 0; i < hash_table->size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+
 	return (hash_table);
 }
