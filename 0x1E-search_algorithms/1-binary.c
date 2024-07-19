cac60e1e917040a861bb8ecd3d174de417917078
PrincewillDev <princewillfidelis1@gmail.com>
2024-07-19 01:15:58 +0100

    Fixed some typo error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,6 +10,7 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left, right, mid;
+
 	left = 0;
 	right = size - 1;
 
@@ -23,17 +24,17 @@ int binary_search(int *array, size_t size, int value)
 		print_array(array, left, right);
 		mid = (left + right) / 2;
 		if (value == array[mid])
-			{
-				return (mid);
-			}
+		{
+			return (mid);
+		}
 		else if (value < array[mid])
-			{
-				right = mid - 1;
-			}
+		{
+			right = mid - 1;
+		}
 		else
-			{
-				left = mid + 1;
-			}
+		{
+			left = mid + 1;
+		}
 	}
 	return (-1);
 }
@@ -48,6 +49,7 @@ int binary_search(int *array, size_t size, int value)
 void print_array(int *array, int left, int right)
 {
 	int i;
+
 	printf("Searching in array: ");
 	for (i = left; i >= left && i <= right; i++)
 	{
